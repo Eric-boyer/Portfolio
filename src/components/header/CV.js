0c3ad6b5948a09ae3381a8e2534aa51ace0094e9
Eric  boyer <ericboyer@MacBook-Pro-de-Eric.local>
2022-06-30 19:43:21 +0200

    portfolio-Version1-CV4

--- src/components/header/CV.js
+++ src/components/header/CV.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import CVERICBOYER3 from "../../assets/CVERICBOYER3.pdf"
+import CVERICBOYER4 from "../../assets/CVERICBOYER4.pdf"
 
 function CV() {
   return (
     <div className='cv'>
-        <a href={CVERICBOYER3} download className='btn'>Telecharger le CV</a>
+        <a href={CVERICBOYER4} download className='btn'>Telecharger le CV</a>
         <a href='#about' className='btn btn-primary'>a propos</a>
     </div>
   )
